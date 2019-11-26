adba645a1bf31d1b7886ee48ac2d26ff48bdbfd0
Daniel Lino De Almeida <ggg4m3r@gmail.com>
2019-11-26 18:15:17 +0000

    add com pasta css e js 2

---
+++ hospital/js/acao.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+})
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+$(document).ready(function(){
+	$(".dropdown-trigger").dropdown();
+})
+$(document).ready(function(){
+	$(".dropdown-trigger").dropdown();
+})
